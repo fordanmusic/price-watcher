95c32c0a11d47bb10a15c7a8a2a8770fc3fd5b52
joost <jspanjerberg@gmail.com>
2020-05-24 22:47:43 +0200

    now checks if source is known before fetching

--- extractdata.py
+++ extractdata.py
@@ -1,7 +1,33 @@
 import re
+import requests
 
 RX_THOMANN_PRICE = '<span class="primary">(.*?)</span>'
 RX_THOMANN_NAME = '<h1 itemprop="name">(.*?)</h1>'
+
+
+
+def determine_source(url):
+    return url.split(".")[1]
+
+def extract_data(url):
+    source = determine_source(url)
+    if source == "thomann":
+        return extract_data_thomann(url)
+    else:
+        raise RuntimeError(f"Unknown source: {source}")
+
+
+
+def fetch_html(url):
+    errmsg = "URL retrieval problem at"
+    fetched = requests.get(url)
+    if fetched.status_code != 200:
+        raise RuntimeError(f"{errmsg} {url}: status {fetched.status_code}")
+    if len(fetched.content) < 50:
+        raise RuntimeError(f"{errmsg} {url}: filesize too small: {len(fetched.content)}")
+    if len(fetched.content) > 1000000:
+        raise RuntimeError(f"{errmsg} {url}: filesize too great: {len(fetched.content)}")
+    return fetched.content.decode('utf-8')
 
 def rx_find(pattern, content):
     found = re.search(pattern, content)
@@ -9,15 +35,12 @@ def rx_find(pattern, content):
         raise RuntimeError(f"Not found")
     return found.group(1)
 
-def extract_data(content, source):
-    if source == "thomann":
-        return extract_data_thomann(content)
-    else:
-        raise RuntimeError(f"Unknown source: {source}")
 
-
-# functions for specific stores
-def extract_data_thomann(content):
+##
+## functions for specific stores
+##
+def extract_data_thomann(url):
+    content = fetch_html(url)
     name = rx_find(RX_THOMANN_NAME, content)
     curr, price = rx_find(RX_THOMANN_PRICE, content).split(" ")
     price = int(price.replace(".", ""))
--- price_watcher.py
+++ price_watcher.py
@@ -10,8 +10,8 @@
 # <span class="primary">€ 3.890</span>
 # <h1 itemprop="name">Yamaha SLB 300 Silent Bass</h1>
 
-import requests
 import datetime
+import json
 from extractdata import extract_data
 
 WATCHLIST = "watch.list"
@@ -20,27 +20,8 @@ def get_list(file):
     with open(file) as f:
         return f.read().splitlines()
 
-def determine_source(url):
-    return url.split(".")[1]
-
-def get_data(url):
-    content = fetch_html(url)
-    source = determine_source(url)
-    return extract_data(content, source)
-
-def fetch_html(url):
-    errmsg = "URL retrieval problem at"
-    fetched = requests.get(url)
-    if fetched.status_code != 200:
-        raise RuntimeError(f"{errmsg} {url}: status {fetched.status_code}")
-    if len(fetched.content) < 50:
-        raise RuntimeError(f"{errmsg} {url}: filesize too small: {len(fetched.content)}")
-    if len(fetched.content) > 1000000:
-        raise RuntimeError(f"{errmsg} {url}: filesize too great: {len(fetched.content)}")
-    return fetched.content.decode('utf-8')
-
 def price_watch(url):
-    product_name, product_price, price_currency = get_data(url)
+    product_name, product_price, price_currency = extract_data(url)
     fetch_date = datetime.datetime.now()
     print(f"{product_name}\n{price_currency} {product_price}\n{fetch_date}\n{url}\n")
 
