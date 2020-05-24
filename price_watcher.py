d47f12649b49eff3c780ad75e256864ec54be1a0
joost <jspanjerberg@gmail.com>
2020-05-24 22:19:13 +0200

    now is able to watch other sources than thomann

--- price_watcher.py
+++ price_watcher.py
@@ -2,7 +2,7 @@
 # integration tests!
 # structure price_fetch object
 # abstract links_list into file?
-    # [prod_name, price_str, price_int, fetch_date]
+    # [prod_name, price_str, price_int, fetch_date, url]
 # can fetch_html be further abstracted?
 
 # usecase:
@@ -14,15 +14,21 @@ import requests
 import re
 import datetime
 
-links_list = (
-    "https://www.thomann.de/intl/yamaha_slb_300_silent_bass.htm",
-    "https://www.thomann.de/intl/aer_bottom_line_amp_one.htm",
-    "https://www.thomann.de/intl/moog_one_16.htm"
-)
+WATCHLIST = "watch.list"
+
+# links_list = (
+#     "https://www.thomann.de/intl/yamaha_slb_300_silent_bass.htm",
+#     "https://www.thomann.de/intl/aer_bottom_line_amp_one.htm",
+#     "https://www.thomann.de/intl/moog_one_16.htm",
+# )
 
 RX_THOMANN_PRICE = '<span class="primary">(.*?)</span>'
 RX_THOMANN_NAME = '<h1 itemprop="name">(.*?)</h1>'
 
+
+def get_list(file):
+    with open(file) as f:
+        return f.read().splitlines()
 
 def fetch_html(url):
     errmsg = "URL retrieval problem at"
@@ -41,16 +47,33 @@ def rx_find(pattern, content):
         raise RuntimeError(f"Not found")
     return found.group(1)
 
-def price_watch_thomann(url):
-    content = fetch_html(url)
+def determine_source(url):
+    return url.split(".")[1]
+
+def extract_data(content, source):
+    if source == "thomann":
+        return extract_data_thomann(content)
+
+def extract_data_thomann(content):
     name = rx_find(RX_THOMANN_NAME, content)
-    price = rx_find(RX_THOMANN_PRICE, content)
-    date = datetime.datetime.now()
-    print(f"{name}\n{price}\n{date}\n{url}\n")
+    curr, price = rx_find(RX_THOMANN_PRICE, content).split(" ")
+    price = int(price.replace(".", ""))
+    return name, price, curr
+
+def fetch_data(url):
+    content = fetch_html(url)
+    source = determine_source(url)
+    return extract_data(content, source)
+
+def price_watch(url):
+    product_name, product_price, price_currency = fetch_data(url)
+    fetch_date = datetime.datetime.now()
+    print(f"{product_name}\n{price_currency} {product_price}\n{fetch_date}\n{url}\n")
+
 
 if __name__ == "__main__":
-    for url in links_list:
+    for url in get_list(WATCHLIST):
         try:
-            price_watch_thomann(url)            
+            price_watch(url)            
         except RuntimeError as e:
             print(f"RuntimeError: {e} -- skipping {url}\n")
