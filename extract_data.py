473a527fd37773e59c4782cca290cae551a036a4
joost <jspanjerberg@gmail.com>
2020-06-03 23:02:13 +0200

    abstracted extract_data_store

--- extract_data.py
+++ extract_data.py
@@ -1,31 +1,13 @@
 import re
 import requests
 
-RX_THOMANN_PRICE = '<span class="primary">(.*?)</span>'
-RX_THOMANN_NAME = '<h1 itemprop="name">(.*?)</h1>'
-RX_THOMANN_AVAIL = '<span class="rs-layover-trigger-text">(.*?)</span>'
+NAME_THOMANN = '<h1 itemprop="name">(.*?)</h1>'
+PRICE_THOMANN = '<span class="primary">(.*?)</span>'
+AVAIL_THOMANN = '<span class="rs-layover-trigger-text">(.*?)</span>'
 
-RX_KIWI_PRICE = '<span class="product-price">(.*?)</span>'
-RX_KIWI_NAME = '<h1 class="heading-title">(.*?)</h1>'
-RX_KIWI_AVAIL = '<span class="journal-stock instock">(.*?)<br ?/>'
-
-
-
-def determine_source(url):
-    try:
-        return re.match('https?://(www\.)?(.*?)\.', url).group(2)
-    except RuntimeError:
-        print(f"URL invalid: {url}")  
-
-def extract_data(url):
-    source = determine_source(url)
-    if source == "thomann":
-        return extract_data_thomann(url)
-    if source == "kiwi-electronics":
-        return extract_data_kiwi(url)
-    else:
-        raise RuntimeError(f"Unknown source: {source}")
-
+NAME_KIWI = '<h1 class="heading-title">(.*?)</h1>'
+PRICE_KIWI = '<span class="product-price">(.*?)</span>'
+AVAIL_KIWI = '<span class="journal-stock instock">(.*?)<br ?/>'
 
 
 def fetch_html(url):
@@ -33,50 +15,69 @@ def fetch_html(url):
     fetched = requests.get(url, timeout=10)
     if fetched.status_code != 200:
         raise RuntimeError(f"{errmsg} {url}: status {fetched.status_code}")
-    if fetched.headers['Content-Type'].split(';')[0]  != 'text/html':
-        raise RuntimeError(f"{errmsg} {url}: content type wrong: {fetched.headers['Content-Type']}")
+    if fetched.headers['Content-Type'].split(';')[0] != 'text/html':
+        raise RuntimeError(
+            f"{errmsg} {url}: content type wrong: {fetched.headers['Content-Type']}")
     if len(fetched.content) < 50:
-        raise RuntimeError(f"{errmsg} {url}: filesize too small: {len(fetched.content)}")
+        raise RuntimeError(
+            f"{errmsg} {url}: filesize too small: {len(fetched.content)}")
     if len(fetched.content) > 1000000:
-        raise RuntimeError(f"{errmsg} {url}: filesize too great: {len(fetched.content)}")
+        raise RuntimeError(
+            f"{errmsg} {url}: filesize too great: {len(fetched.content)}")
     return fetched.content.decode('utf-8')
 
-def rx_find(pattern, content):
-    found = re.search(pattern, content)
-    if not found:
-        raise RuntimeError(f"Not found: {pattern}")
-    return found.group(1)
-
-def get_currency(symbol):
-    if symbol == '\u20ac':
-        return "EUR"
-    else: return None
 
 def strip_html(line):
     line = re.sub('</?b>', '', line)
     line = re.sub('</?span(.*?)>', '', line)
     return line
-##
-## functions for specific stores
-##
-def extract_data_thomann(url):
+
+
+def rx_find(pattern, content):
+    found = re.search(pattern, content)
+    if not found:
+        raise RuntimeError(f"Not found: {pattern}")
+    return strip_html(found.group(1))
+
+
+def get_price(price_str):
+    price_str = price_str.replace(".", "")
+    price_str = price_str.replace(",", ".")
+    return float(price_str)
+
+
+def get_currency(symbol):
+    if symbol == '\u20ac':
+        return "EUR"
+    else:
+        return None
+
+
+def extract_data_store(url, NAME_RX, PRICE_RX, AVAIL_RX):
     content = fetch_html(url)
-    name = rx_find(RX_THOMANN_NAME, content)
-    curr_sym, price = rx_find(RX_THOMANN_PRICE, content).split(" ")
-    avail = rx_find(RX_THOMANN_AVAIL, content)
-    price = float(price.replace(".", ""))
+    name = rx_find(NAME_RX, content)
+    curr_sym, price = rx_find(PRICE_RX, content).split(" ")
+    avail = rx_find(AVAIL_RX, content)
+    price = get_price(price)
     curr = get_currency(curr_sym)
     return name, price, curr, avail
 
-def extract_data_kiwi(url):
-    content = fetch_html(url)
-    name = rx_find(RX_KIWI_NAME, content)
-    curr_sym, price = rx_find(RX_KIWI_PRICE, content).split(" ")
-    avail = strip_html(rx_find(RX_KIWI_AVAIL, content))
-    price = float(price.replace(",", "."))
-    curr = get_currency(curr_sym)
-    return name, price, curr, avail
 
+def determine_source(url):
+    try:
+        return re.match('https?://(www\.)?(.*?)\.', url).group(2)
+    except RuntimeError:
+        print(f"URL invalid: {url}")
+
+
+def extract_data(url):
+    source = determine_source(url)
+    if source == "thomann":
+        return extract_data_store(url, NAME_THOMANN, PRICE_THOMANN, AVAIL_THOMANN)
+    if source == "kiwi-electronics":
+        return extract_data_store(url, NAME_KIWI, PRICE_KIWI, AVAIL_KIWI)
+    else:
+        raise RuntimeError(f"Unknown source: {source}")
 
 
 if __name__ == "__main__":
--- price_watcher.py
+++ price_watcher.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python3
 
-# STILL TO DO
-# integration tests!
-# error log
-# make paths relative to where file is located
-# on pi: crontab mailto
-# on pi: git
-
 import json
 import os
 from pathlib import Path
 from datetime import datetime
-from extractdata import extract_data
+from extract_data import extract_data
 
 ROOT = str(Path(os.path.abspath(os.path.realpath(__file__))).parent)
 WATCHLIST = ROOT + "/watch.list"
@@ -20,29 +13,30 @@ LOGROOT = ROOT + "/logs/"
 
 def get_list(file):
     with open(file) as f:
-        return [line for line in f.read().splitlines() if line and not line.startswith('#')]
-    
+        return [line for line in f.read().splitlines()
+                if line and not line.startswith('#')]
+
+
 def price_watch(url):
     product_name, product_price, currency, avail = extract_data(url)
     timestamp = str(datetime.now())
-    json_dict = { "Product name": product_name,
-                  "Product price": product_price,
-                  "Currency": currency,
-                  "Availability": avail,
-                  "URL": url,
-                  "Timestamp": timestamp }
-    #print(f"Fetched: {url}")
+    json_dict = {"Product name": product_name,
+                 "Product price": product_price,
+                 "Currency": currency,
+                 "Availability": avail,
+                 "URL": url,
+                 "Timestamp": timestamp}
     print(f"{product_name:50}{product_price:8} {currency:5}({avail})")
-    #print()
     return json_dict
 
+
 if __name__ == "__main__":
-    date = str(datetime.now().date()) 
-    logfile = LOGROOT + date + ".json"
+    date = str(datetime.now().date())
+    logfile = LOGROOT + "pw-" + date + ".json"
     with open(logfile, 'a+') as f:
         for url in get_list(WATCHLIST):
             try:
                 json.dump(price_watch(url), f)
-                f.write('\n')            
+                f.write('\n')
             except RuntimeError as e:
                 print(f"RuntimeError: {e}\n-- skipping {url}\n")
